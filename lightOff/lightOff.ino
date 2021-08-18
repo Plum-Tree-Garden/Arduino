88cd247945e93947c1253bbce01152f9bec9cd0f
Plum-Tree-Garden <73845528+Plum-Tree-Garden@users.noreply.github.com>
2021-08-18 00:10:09 -0400

    Create lightOff.ino

---
+++ lightOff/lightOff.ino
@@ -0,0 +1,23 @@
+// plumtreegarden
+// lightOff.ino
+
+void setup() 
+{
+  // tell arduino what pin(s) working with: pin 13  
+  // pinMode take 2 paramaters(pin# working with, input / output)
+  // send signal so it will be output -> pin 13 becomes out
+  pinMode(13,OUTPUT);
+}
+
+void loop() 
+{
+  // TURN LED ON = ON = 5V, TURN LED OFF = LOW
+  // 2 paramaters - (pin to write to, on / off)
+  // TODO   MAKE BLINKING LED
+  // TODO   ADD DELAY BETWEEN ON / OFF
+  // delay -> 1 -> PARAMATER = MILISECONDS -> 1000 MS = 1S
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); 
+  delay(1000); 
+}
