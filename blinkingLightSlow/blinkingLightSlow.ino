5bd64cae0937c44647b90d50ec00ce16b97f2ad8
Plum-Tree-Garden <73845528+Plum-Tree-Garden@users.noreply.github.com>
2021-08-18 00:12:22 -0400

    Update blinkingLightSlow.ino

--- blinkingLightSlow/blinkingLightSlow.ino
+++ blinkingLightSlow/blinkingLightSlow.ino
@@ -1,20 +1,27 @@
 // plumtreegarden
 // blinkingLightSlow.ino
 
-int j=1;
-int waitT=750;
-String myString="j =";
+// plumtreegarden
 
 
 void setup() 
 {
-  Serial.begin(9600);
+  // tell arduino what pin(s) working with: pin 13  
+  // pinMode take 2 paramaters(pin# working with, input / output)
+  // send signal so it will be output -> pin 13 becomes out
+  pinMode(13,OUTPUT);
 }
 
 void loop() 
 {
-  // print integer and string with a delay time of 750 miliseconds
-  Serial.print(myString);
-  Serial.println(j);
-  delay(waitT);
+  // TURN LED ON = ON = 5V, TURN LED OFF = LOW
+  // 2 paramaters - (pin to write to, on / off)
+  // TODO   MAKE BLINKING LED
+  // TODO   ADD DELAY BETWEEN ON / OFF
+  // delay -> 1 -> PARAMATER = MILISECONDS -> 1000 MS = 1S
+  // TODO - FAST BLINKING LIGHT
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW); 
+  delay(900); 
 }
